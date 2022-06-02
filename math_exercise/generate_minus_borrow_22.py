870238ec19ceb7e8cf13a23861e8dbaca8a9e633
a <a@b.c>
2022-06-02 08:27:26 -0700

    new file

---
+++ math_exercise/generate_minus_borrow_22.py
@@ -0,0 +1,30 @@
+import random
+import configparser
+import list_print
+
+cfg = configparser.ConfigParser()
+cfg.read("./env.ini")
+result_file   = cfg.getint('option', 'result_file')
+result_chunk  = cfg.getint('option', 'result_chunk')
+result_repeat = cfg.getint('option', 'result_repeat')
+
+list_ones = []
+for i in range(9):
+    for j in range(i+1, 10):
+        list_ones.append([i,j])
+
+list_tens = []
+for i in range(2, 10):
+    for j in range(1, i):
+        list_tens.append([i,j])
+
+for i in range(result_file):
+    fname = "result_{0}.txt".format(i)
+    with open(fname, 'w') as f:
+        for repeat in range(result_repeat):
+            result = []
+            for j in range(result_chunk):
+                choice_one = random.choice(list_ones)
+                choice_ten = random.choice(list_tens)
+                result.append([choice_ten[0]*10+choice_one[0], choice_ten[1]*10+choice_one[1]])
+            list_print.list_print(f, result_chunk, 0, result)
--- math_exercise/list_print.py
+++ math_exercise/list_print.py
@@ -5,8 +5,8 @@ def list_print(f, chunk, sign, result):
         s_sign = '-'
 
     for j in range(chunk):
-        f.write('\t\t{0}'.format(result[j][0]))
+        f.write('\t\t\t{0}'.format(result[j][0]))
     f.write('\n')
     for j in range(chunk):
-        f.write('\t{0}\t{1}'.format(s_sign, result[j][1]))
-    f.write('\n')
+        f.write('\t\t{0}\t{1}'.format(s_sign, result[j][1]))
+    f.write('\n\n\n')
